b0444927173d4c1e67f1116d2cee04aca270c30c
Abubakr Masood <68956896+abm98@users.noreply.github.com>
2021-01-28 19:32:38 +0530

    Update Face_Detector.py

--- Face_Detector.py
+++ Face_Detector.py
@@ -1,10 +1,12 @@
 import cv2
 from random import randrange
+
 #load some pre trained data on face frontal from opencv_storage(haar cascade algorithms)
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # choose image to detect face
 #img = cv2.imread('Beard.jpg')
+
 # To detect image from webcam
 webcam = cv2.VideoCapture('kyatum.mp4')  # if argument is given as 0 it will run through your webcam otherwise as per given file in quote
 
@@ -13,16 +15,15 @@ while True:
     # Read the current frame
     successful_frame_read, frame = webcam.read()
 
-
-#Convert to Grayscale
+    #Convert to Grayscale
     grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#Detect face
+    #Detect face
     face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
                     #(grayscaled_img,2,6) you can change the value to change senstivity of detection
-# Draw rectangle around face
+    # Draw rectangle around face
     for (x, y, w, h) in face_coordinates:
-    # (0,0,0) change for color / ,4) change thickness
+    # (0,0,0) change for color / ,4) change thickness of outline 
         cv2.rectangle(frame,(x,y),(x+w, y+h), (randrange(255),randrange(255),randrange(255)),4)
 
  # if you are using single image and there is multiple pic change the array value and rectangle will replace to next image
@@ -38,7 +39,3 @@ while True:
         break
 # Release the video object which is capture
 webcam.release()
-
-
-
-print("Masood")
