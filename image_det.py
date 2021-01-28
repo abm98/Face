e355f502a9a29b9e2648d8f8ad9971f1e08ab34e
Abubakr Masood <68956896+abm98@users.noreply.github.com>
2021-01-28 19:44:21 +0530

    Update image_det.py

--- image_det.py
+++ image_det.py
@@ -2,6 +2,7 @@ import cv2
 from random import randrange
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+#Read the image
 img = cv2.imread('Beard.jpg')
 
 #Convert to Grayscale
@@ -12,9 +13,10 @@ face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 
 # Draw rectangle around face
 for (x, y, w, h) in face_coordinates:
-    # (0,0,0) change for color / ,4) change thickness
+    # (0,0,0) change for color / ,4) change thickness randrange is used for random color change
     cv2.rectangle(img,(x,y),(x+h, y+w), (randrange(255),randrange(255),randrange(255)),4)
-cv2.imshow('ABM Face Detector', img)
+
+# Show the Image    
+cv2.imshow('ABM Image Detector', img)
 cv2.waitKey()
 print(face_coordinates)
-print("Mas")
