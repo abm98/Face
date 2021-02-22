86e21ac3a20828c48a434dabd6f6c02579a8d9c7
Abubakr Masood <68956896+abm98@users.noreply.github.com>
2021-02-22 18:51:00 +0530

    Update image_det.py

--- image_det.py
+++ image_det.py
@@ -3,13 +3,14 @@ from random import randrange
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Read the image
-img = cv2.imread('Beard.jpg')
+image = cv2.imread('abc.jpg')
 
 #Convert to Grayscale
-grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+grayscaled_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #Detect face
-face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_image)
+print("Found {0} Face!".format(len(face_coordinates)))
 
 # Draw rectangle around face
 for (x, y, w, h) in face_coordinates:
@@ -17,6 +18,6 @@ for (x, y, w, h) in face_coordinates:
     cv2.rectangle(img,(x,y),(x+h, y+w), (randrange(255),randrange(255),randrange(255)),4)
 
 # Show the Image    
-cv2.imshow('ABM Image Detector', img)
+cv2.imshow('ABM Image Detector', image)
 cv2.waitKey()
 print(face_coordinates)
